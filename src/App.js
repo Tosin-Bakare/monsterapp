a5be8a369a52d264e8f263082eac8e1aa23b5427
Oluwatosin Bakare <oluwatosin.bakare@yahoo.com>
2021-02-17 11:17:35 +0100

    Monster Rolodex App filtering done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 
@@ -21,14 +22,17 @@ class App extends React.Component {
 
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <input
-          type='search'
-          placeholder='search monsters'
-          onChange={e => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder='search monster'
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
